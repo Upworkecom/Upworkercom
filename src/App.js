2140d45ed892a8b7f0078437175abf4e73a29d95
flooff <flo133.dillenschneider@gmail.com>
2021-11-30 20:39:03 +0100

    HeroSection

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
+import Home from "./components/pages/Home";
+
+
 
 function App() {
   return (
-    <>
-    <Router>
-    <Navbar />
-      <Routes>
-        <Route path='/' exact />
-      </Routes>
-    </Router>
+      <>
+      <Router>
+        <Navbar />
+        <Home />
+        <Routes>
+          <Route path='/' exact/>
+        </Routes>
+      </Router>
     </>
   );
 }
