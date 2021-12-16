7c70ed7a43da7c810f787eb31e2e7e41113974eb
flooff <flo133.dillenschneider@gmail.com>
2021-12-16 14:45:50 +0100

    cleaning

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,13 +2,12 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
-import video from '../videos/video-2.mp4'
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src={video} type='video/mp4' autoPlay loop muted />
-      <h1> ULTIMATE  SCRAPPER</h1>
+      <video src='/videos/video-2.mp4' autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
         <Button
@@ -24,7 +23,7 @@ function HeroSection() {
           buttonSize='btn--large'
           onClick={console.log('hey')}
         >
-          More Info 
+          WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,12 @@ import './Navbar.css';
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
-  const [navbar, setNavbar] = useState(false)
- 
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if (window.innerWidth <= 760) {
+    if (window.innerWidth <= 960) {
       setButton(false);
     } else {
       setButton(true);
@@ -25,23 +24,13 @@ function Navbar() {
 
   window.addEventListener('resize', showButton);
 
-  const changeBackground = () =>{
-    if(window.scrollY >= 80){
-      setNavbar(true)
-    }else{
-      setNavbar(false)
-    }
-  }
-
-  window.addEventListener('scroll',changeBackground)
-
   return (
     <>
-      <nav className={navbar ? 'navbar active': 'navbar'}>
+      <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            Upworker
-            <i className='fab fa-typo3' />
+            TRVL
+            <i class='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
