3b521d98729188e23cb1c8a1df97dd711859bb25
flooff <flo133.dillenschneider@gmail.com>
2021-12-22 15:34:10 +0100

    Sign

--- src/components/signUp/signUp.js
+++ src/components/signUp/signUp.js
@@ -22,7 +22,7 @@ export default function Signup() {
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               Or{' '}
-              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+              <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500">
                 start your 14-day free trial
               </a>
             </p>
@@ -74,7 +74,7 @@ export default function Signup() {
               </div>
 
               <div className="text-sm">
-                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500">
                   Forgot your password?
                 </a>
               </div>
